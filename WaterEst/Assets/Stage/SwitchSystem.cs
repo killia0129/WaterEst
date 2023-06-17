4abe881c5d6500276324b8929992867a28f45989
yonemoriharuki <21080021yh@edu.fca.ac.jp>
2023-06-17 11:31:04 +0900

    スイッチ完成

--- WaterEst/Assets/Stage/SwitchSystem.cs
+++ WaterEst/Assets/Stage/SwitchSystem.cs
@@ -15,35 +15,40 @@ public class SwitchSystem : MonoBehaviour
     float repeatSpan = 8;
     float waterStopElapsedTime = 0;
     float bottomPosY;
+    bool switchReset;
     // Start is called before the first frame update
     void Start()
     {
         repeatSpan = 7;
         waterStopElapsedTime = 0;
         bottomPosY = transform.position.y;
+        switchReset = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        waterStopElapsedTime += Time.deltaTime;
+    { 
         if (active && transform.position.y > bottomY)
         {
             transform.position -= Vector3.up * speed * Time.deltaTime;
+           
         }
         if (active)
         {
             WaterShotScript.waterActive = false;
+            switchReset = false;
+            waterStopElapsedTime += Time.deltaTime;
         }
         if(waterStopElapsedTime >= repeatSpan)          //押された状態から元に戻す
         {
             active = false;
             WaterShotScript.waterActive = true;
             waterStopElapsedTime = 0;
+            switchReset = true;
         }
-        if(active == false && transform.position.y < bottomPosY)    //★押すタイミングによって戻る速さが変わるバグを直す
+        if (active == false && transform.position.y < bottomPosY&&switchReset == true)    //★押すタイミングによって戻る速さが変わるバグを直す
         {
-         transform.position += Vector3.up * speed * Time.deltaTime;
+            transform.position += Vector3.up * speed * Time.deltaTime;
         }
 
         Debug.Log(waterStopElapsedTime);
